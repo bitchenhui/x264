8d5be3df239d37f29df76b8840a3c53526c69b44
bitchenhui <13261281092@163.com>
2023-08-28 17:37:56 +0800

    feat:improve script

--- x264_ios_build.sh
+++ x264_ios_build.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+###
+ # @Author: zhangda
+ # @Date: 2023-08-28 10:29:49
+ # @LastEditors: 段晨辉 duanchenhui@xiaohongshu.com
+ # @LastEditTime: 2023-08-28 17:36:47
+ # @FilePath: /codeccmakebuild/x264enc/x264/x264_ios_build.sh
+ # @Description: 
+ # 
+ # Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
+### 
 
 export PLATFORM="iPhoneOS"
 export MIN_VERSION="10.0"
@@ -12,13 +22,15 @@ function build_x264()
     export DEVROOT=/Applications/Xcode.app/Contents/Developer/Platforms/${PLATFORM}.platform/Developer
     export SDKROOT=$DEVROOT/SDKs/${PLATFORM}${SDK_VERSION}.sdk
     export CFLAGS="-std=c99 -arch ${ARCH} -isysroot ${SDKROOT} -O3 -DNDEBUG -miphoneos-version-min=${MIN_VERSION}"
-    # export LDFLAGS="-L${SDKROOT}/usr/lib/"
+    export LDFLAGS="-L${SDKROOT}/usr/lib"
     export CC=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang
     export CXX=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang++
     
-    make clean
     ./configure --host=aarch64-apple-darwin --sysroot=$SDKROOT --prefix="$OUTPUT/${PLATFORM}${SDK_VERSION}-${ARCH}.sdk" \
-    --extra-cflags="-arch ${ARCH} -isysroot ${SDKROOT}" --extra-ldflags="-arch ${ARCH} ${LDFLAGS}" --enable-pic
+    --extra-cflags="-arch ${ARCH} -isysroot ${SDKROOT}" --extra-ldflags="-arch ${ARCH} ${LDFLAGS}" --enable-pic --enable-static \
+    --enable-shared --bit-depth=8 --disable-interlaced --disable-cli
+    make clean
     make && make install
+    make clean
 }
 build_x264
